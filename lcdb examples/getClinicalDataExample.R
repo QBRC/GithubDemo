7959b1ae4a49772a7ae276015d6909e64dc96de2
Jeff Allen <jeff.allen@trestletechnology.net>
2012-12-10 16:48:01 -0600

    Provided example for getting patients and samples.

--- lcdb examples/getClinicalDataExample.R
+++ lcdb examples/getClinicalDataExample.R
@@ -1,1 +1,14 @@
 library(lcdb)
+
+LCDBServer <- lcdb.server("qbri.swmed.edu")
+
+#Get all clinical information in the databse
+allPatients <- lcdb.getPatients(LCDBServer)
+
+#Get all the samples associated with the Consortium Dataset (#14)
+consortiumSamples <- lcdb.getSamples(LCDBServer, DatasetID = 14)
+head(consortiumSamples)
+
+#Get all patients associated with the above samples.
+consortiumPatients <- lcdb.getPatients(LCDBServer, PatientID = consortiumSamples$PatientID)
+head(consortiumPatients)
