6882c9b90a1a2a795b2fff38ec9b115119f06f34
Jeff Allen <jeff.allen@trestletechnology.net>
2012-12-10 16:45:03 -0600

    Retrieve all data from dataset 14.

--- lcdb examples/summarizeGenes.R
+++ lcdb examples/summarizeGenes.R
@@ -1,1 +1,16 @@
+library(probemapper)
+library(lcdb)
+
+PMServer <- SOAPServer("qbri.swmed.edu", "ProbeMapperWS/services/ProbeMapperSOAP", 80)
+LCDBServer <- lcdb.server("qbri.swmed.edu")
+
+#get all expression data from dataset #14 (consortium) of normalization type 2 (reprocessed)
 rd2 <- readRDS("/home/data/QBRI/LCDB_Exports/14-n2.rds")
+
+#Get in increments of 1,000 and reassemble to avoid overwhelming the web server
+probeToGene <- pm.getGenesByProbe(PMServer, rownames(rd2)[1])
+for (i in seq(2, nrow(rd2), by=1000)){
+  print(i)
+  p2g <- pm.getGenesByProbe(PMServer, rownames(rd2)[i:min(i+999, nrow(rd2))])
+  probeToGene <- rbind(probeToGene, p2g)
+}
