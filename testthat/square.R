9dfa951474f8ed765465c63433503aaf7f3cbeb6
Jeff Allen <jeff.allen@trestletechnology.net>
2013-03-26 14:12:44 -0500

    Moved to multiply method
    Closes #1.

--- testthat/square.R
+++ testthat/square.R
@@ -1,15 +1,15 @@
 library(testthat)
 
+#' Expect that x is numeric
 square <- function(x){
-	sq <- 0
-	for (i in 1:x){
-		sq <- sq + x
-	}
+	sq <- x*x
 	return(sq)	
 }
 
 test_that("Square function works on various input types", {
 	expect_that(square(3), equals(9))
 	expect_that(square(5), equals(25))	
+  expect_that(square(2.5), equals(6.25))
+  expect_that(square(-2), equals(4))
 })
 
