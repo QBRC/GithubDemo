b047baa4e73e936532d7968f87922ddc7dcaaae7
trestletech <jeff.allen@trestletechnology.net>
2013-03-25 22:45:10 -0500

    Added testthat demonstration.

---
+++ testthat/square.R
@@ -0,0 +1,13 @@
+square <- function(x){
+	sq <- 0
+	for (i in 1:x){
+		sq <- sq + x
+	}
+	return(sq)	
+}
+
+test_that("Square function works on various input types", {
+	expect_that(square(3), equals(9))
+	expect_that(square(5), equals(25))	
+})
+
